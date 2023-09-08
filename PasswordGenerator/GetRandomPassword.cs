8b24b1c369e9cd76b24f241210b4f4537926010c
Electrum <58157207+Electrum-Data@users.noreply.github.com>
2023-09-07 17:42:23 -0700

    Initial Commit

---
+++ PasswordGenerator/GetRandomPassword.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+// special: [33 - 47], [58 - 64], [91 - 96], [123 - 126]
+// numbers: 48 - 57
+// upper: 65 - 90
+// lower: 97 - 122
+
+namespace PasswordGenerator
+{
+    internal class GetRandomPassword
+    {
+        public static string GetRandom(int length, int selection)
+        {
+            var random = new Random();
+  
+            string pw = "";
+
+            for (int i = 0; i < length; i++)
+            {
+                int[] upperLower = { random.Next(65, 90), random.Next(97, 122) };
+                int[] upperLowerNums = { random.Next(65, 90), random.Next(97, 122), random.Next(48, 57) };
+                int[] upperLowerSpecs = { random.Next(65, 90), random.Next(97, 122), random.Next(33, 47), random.Next(58, 64), random.Next(91, 96), random.Next(123, 126) };
+                int[] all = { random.Next(65, 90), random.Next(97, 122), random.Next(33, 47), random.Next(58, 64), random.Next(91, 96), random.Next(123, 126), random.Next(48, 57) };
+
+                int[] choice;
+                choice = selection switch
+                {
+                    1 => choice = upperLower,
+                    2 => choice = upperLowerNums,
+                    3 => choice = upperLowerSpecs,
+                    4 => choice = all,
+                    _ => throw new NotImplementedException()
+                };
+
+                pw += (char)choice[random.Next(0, choice.Length)];
+            }
+
+            return pw;
+        }
+    }
+}
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,42 @@
+﻿using PasswordGenerator;
+using System.Collections.Generic;
+
+var start = true;
+while (start)
+{
+    Console.Write("Select password length between 8 and 20: ");
+    var length = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+
+    if (length > 0 && length < 21)
+    {
+        while(true)
+        {
+            Console.Write("----------------- " +
+                        "\nWHAT TO INCLUDE " +
+                        "\n----------------- " +
+                        "\n1: Uppercase + Lowercase" +
+                        "\n2: Include Numbers" +
+                        "\n3: Inlcude Special Characters" +
+                        "\n4: Include Everything" +
+                        "\nMake a selection: ");
+            var selection = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            if (selection > 0 && selection < 5)
+            {
+                var password = GetRandomPassword.GetRandom(length, selection).ToString();
+                Console.WriteLine();
+                Console.WriteLine($"Password: {password}");
+                start = false;
+                break;
+            }else
+            {
+                continue;
+            }
+        }
+    }else
+    {
+        continue;
+    }
+
+}
